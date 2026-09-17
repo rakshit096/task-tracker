f0f6d45cc67dea80bbe56310aeba8df56e3f4671
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-17 18:34:30 +0530

    feat: add user assignee association, enum, validation to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :project
+  belongs_to :assignee, class_name: "User", optional: true
+  enum :status, { pending: 0, in_progress: 1, done: 2 }, default: :pending
+
+  validates :title, presence: true
 end
