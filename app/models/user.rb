2ca53f1766cc5a640a22332f71f420de123fc6ad
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-17 18:19:00 +0530

    feat: add project and task association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  has_many :projects, dependent: :destroy
+  has_many :assigned_tasks, class_name: "Task", foreign_key: :assignee_id, dependent: :nullify # if user is deleted. Dont delete their specified task
+
+
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
