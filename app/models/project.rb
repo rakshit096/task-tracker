6a95cd9c482c29cd61814537f803fc32a6c3f484
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-17 18:21:40 +0530

    feat: add task association in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :tasks, dependent: :destroy   #if project is deleted then the tasks under it gets deleted
+
+  validates :name, presence: true
 end
