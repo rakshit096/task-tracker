52f9ea2e91e28d7fac84f907e4e1b9b954358c79
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-18 10:52:13 +0530

    feat: add root route to projects, add projects controller and views

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[ show edit update destroy ]
+
+  def index
+    @projects = Current.user.projects
+  end
+
+  def show
+  end
+
+  def new
+    @project = Current.user.projects.new
+  end
+
+  def create
+    @project = Current.user.projects.new(project_params)
+    if @project.save
+      redirect_to @project, notice: "Project created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path, notice: "Project deleted."
+  end
+
+  private
+
+  def set_project
+    @project = Current.user.projects.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  root "sessions#new"
+  root "projects#new"
 
   resource :session
   resources :passwords, param: :token
   resource :registration, only: %i[ new create ]
+  resources :projects
+
 
 end
