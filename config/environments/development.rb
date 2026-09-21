eb0cdc1e67bbeb755631e4ccc239ab501daeb945
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-21 11:07:15 +0530

    add test job process inline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
   config.server_timing = true
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.active_job.queue_adapter = :inline
+
 
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
