1e94aeb7efcff7465902ea8a4beb001205c7eaab
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-21 13:45:07 +0530

    feat: add task tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @project = Project.create!(name: "Test Project", user: @user)
+  end
+
+  test "valid with a title and project" do
+    task = Task.new(title: "Do something", project: @project)
+    assert task.valid?
+  end
+
+  test "invalid without a title" do
+    task = Task.new(title: "", project: @project)
+    assert_not task.valid?
+    assert_includes task.errors[:title], "can't be blank"
+  end
+
+  test "defaults to pending status" do
+    task = Task.create!(title: "New task", project: @project)
+    assert task.pending?
+  end
+
+  test "can be assigned to a user" do
+    task = Task.create!(title: "Assigned task", project: @project, assignee: @user)
+    assert_equal @user, task.assignee
+  end
+
+  test "is valid without an assignee" do
+    task = Task.new(title: "Unassigned task", project: @project)
+    assert task.valid?
+  end
+
+  test "enqueues a notification job when assignee changes" do
+    task = Task.create!(title: "Task", project: @project)
+    assert_enqueued_with(job: TaskAssignmentNotifierJob) do
+      task.update!(assignee: @user)
+    end
+  end
+
+  test "does not enqueue a job when assignee is unchanged" do
+    task = Task.create!(title: "Task", project: @project, assignee: @user)
+    assert_no_enqueued_jobs only: TaskAssignmentNotifierJob do
+      task.update!(title: "Renamed")
+    end
+  end
+    include ActiveJob::TestHelper
 end
