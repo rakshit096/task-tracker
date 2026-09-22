0c1b214018510e71f9b13cc876ef1e50216c87df
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-22 10:58:48 +0530

    feat: add assigned task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,12 @@
 class TasksController < ApplicationController
-  before_action :set_project
-  before_action :set_task, only: %i[ edit update destroy update_status ]
+  before_action :set_project, except: %i[ assigned_to_me ]
+  before_action :set_task, only: %i[ show edit update destroy update_status ]
+  before_action :authorize_viewer!, only: %i[ show update_status ]
+  before_action :authorize_owner!, only: %i[ new create edit update destroy ]
+  skip_before_action :set_project, only: :assigned_to_me
+
+  def show
+  end
 
   def new
     @task = @project.tasks.new
@@ -29,9 +35,9 @@ class TasksController < ApplicationController
   def update_status
     @task.update(status: params[:status])
     respond_to do |format|
-    format.turbo_stream
-    format.html { redirect_to @project }
-   end
+      format.turbo_stream
+      format.html { redirect_to @project }
+    end
   end
 
   def destroy
@@ -39,14 +45,29 @@ class TasksController < ApplicationController
     redirect_to @project, notice: "Task deleted."
   end
 
+  def assigned_to_me
+    @tasks = Current.user.assigned_tasks.includes(:project)
+  end
+
   private
 
   def set_project
-    @project = Current.user.projects.find(params[:project_id])
+  
+    @project = Project.find(params[:project_id])
   end
 
   def set_task
     @task = @project.tasks.find(params[:id])
+  end
+
+  def authorize_viewer!
+    unless @project.user == Current.user || @task.assignee == Current.user
+      head :not_found
+    end
+  end
+
+  def authorize_owner!
+    head :not_found unless @project.user == Current.user
   end
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
   resource :registration, only: %i[ new create ]
 
   resources :projects do                       #tasks are nested under projects and it generates all restful actions except the two mentioned
-    resources :tasks, except: [:index, :show] do
+    resources :tasks, except: [] do
       member do
         patch :update_status
       end
     end
   end
+
+  get "my_tasks", to: "tasks#assigned_to_me"
 end
